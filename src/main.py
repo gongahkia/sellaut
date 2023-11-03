1371176944a69f6da7b3cff2549fed45925a4781
gongahkia <angryapplegravy@gmail.com>
2023-11-03 16:38:30 +0800

    default filler message

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import os
 
 # basic info
 # - integrate curses CLI and integrate existing file_ui method into it
-# - screen size: [0,0] to [60,25]
+# - screen size: [0,0] to [60,25], a 61 by 26 grid
 # - add colors to the screen
 # - implement interactions between elements
 # - implement solid dynamics (gravity), liquid dynamics (gravity and liquid fluidity) and gas dynamics
@@ -40,12 +40,12 @@ def file_ui() -> [str]:
     fhand = open(f"{valid_file_name[int(user) - 1]}", "r")
     buffer:list = []
     for line in fhand:
-        if len(line.rstrip()) != 60:
+        if len(line.rstrip()) != 61:
             return None
         # print(line, end="")
         buffer.append(line.rstrip())
     fhand.close()
-    if len(buffer) != 25:
+    if len(buffer) != 26:
         return None
     return buffer
 
@@ -54,7 +54,7 @@ def parse_file() -> [dict]:
     buffer:[str] = file_ui()
     if not buffer:
         os.system("clear")
-        print("Incorrect number of rows and columns. Please input a 60 by 25 grid.")
+        print("Incorrect number of rows and columns. Please input a 61 by 26 grid.")
         return None
     coord:[dict] = []
     for y in range(len(buffer)):
@@ -93,11 +93,76 @@ def parse_file() -> [dict]:
                     print(f"Unknown character found in line {y + 1} --> [{buffer[y][x]}]")
                     return None
             coord.append(cell_data)
+            # debug(coord)
     return coord
 
-# debug information
-# FUA - add details to this debug function that prints out the coordinates to a dot file called .log or somthing 
-def debug([dict]) -> None:
+# prints debug information
+def debug(coord:[dict]) -> None:
+    fhand = open(".sellaut-log","w")
+    final:str = ""
+    for cell_info in coord:
+        final += f"{str(cell_info)}\n"
+    fhand.write(final)
+    fhand.close()
+
+# incorporates curses module
+# FUA --> get the update function later to determine if there are no changes in state after 10 cycles, to terminate the program
+def render(coord:[dict]) -> None:
+
+    # curses boilerplate
+    screen = curses.initscr()
+    curses.cbreak()
+    curses.noecho()
+    screen.timeout(0)
+    curses.curs_set(0)
+
+    if curses.has_colors():
+        curses.start_color()
+        
+        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+        curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+        curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+        curses.init_pair(6, curses.COLOR_CYAN, curses.COLOR_BLACK)
+        curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+
+        for cell in coord:
+            x_coord:int = cell["coordinate"][0]
+            y_coord:int = cell["coordinate"][1]
+            ascii_char:str = ""
+            match cell["element"]:
+                case "air_block":
+                    ascii_char = "."
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(7))
+                case "flammable_block": 
+                    ascii_char = "$"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(2))
+                case "oil_block": 
+                    ascii_char = "*"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(4))
+                case "fire_block": 
+                    ascii_char = "^"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(1))
+                case "water_block": 
+                    ascii_char = "≈"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(6))
+                case "non-flammable_block": 
+                    ascii_char = "#"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(3))
+                case "soot_block": 
+                    ascii_char = "_"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(5))
+                case "effervesence_block": 
+                    ascii_char = "%"
+                    screen.addstr(y_coord, x_coord, ascii_char, curses.color_pair(7))
+                case _: 
+                    os.system("clear")
+                    print(f"Edge case 001 found.")
+                    return None
+        screen.refresh() # refreshes the screen once all cells have been added
+        curses.napms(10000) # waits few seconds without input
+        curses.endwin() # exits curses window
 
 # event loop
-print(parse_file())
+render(parse_file())
